23f2b096c1dfa7b7759e8a767148b30da6568bd0
chaimchessrak-vayyar <chaim.chessrak@vayyar.com>
2020-12-08 23:37:35 +0200

    Lot of improvements, and new statistics on accumulative distributions.

--- reverse_order.py
+++ reverse_order.py
@@ -2,9 +2,18 @@
 from matplotlib import pyplot as plt
 from pathlib import Path
 import datetime
+import numpy as np
 
 debug = False
 results_path = Path(r'.\NLP_backward_bigrams_Results')
+counter_key = 'All_appearances_ooooooo'
+
+graph_resolution = 60
+rare_word_threshold = 31
+number_of_words_global = 4
+no_mass = -9999
+now_results_dir = results_path / f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+Path.mkdir(now_results_dir)
 
 
 def runner():
@@ -15,19 +24,52 @@ def runner():
     for idx, file_path in enumerate(text_dir_path.iterdir()):
         print(f'{idx}: {file_path}')
         main(file_path, forward_dict, backward_dict)
+    statistics(forward_dict, backward_dict)
+    return None
+
+
+def statistics(forward_dict, backward_dict):
+    make_avg_accumulative_distribution_plot(forward_dict, backward_dict)
     make_distributions(forward_dict, backward_dict)
     return 0
+
+
+def make_avg_accumulative_distribution_plot(forward_dict, backward_dict):
+    accumulative = [accumul(forward_dict, backward_dict, i) for i in range(0, rare_word_threshold)]
+    forward = [x for x, y in accumulative]
+    backward = [y for x, y in accumulative]
+    ratio = [back / forw if forw != 0 else 0 for back, forw in zip(backward, forward)]
+    indices = list(range(0, rare_word_threshold))
+    plt.plot(indices, forward, label='forward')
+    plt.plot(indices, backward, label='backward')
+    plt.plot(indices, ratio, label='ratio')
+    plt.title('Average accumulative distribution of first K most frequent words')
+    plt.legend()
+    plt.savefig(now_results_dir / 'accumulative_distributions_ratio.png')
+    plt.show()
+
+
+def accumul(forward_dict, backward_dict, i):
+    mass_vector_forward = calculate_mass_distribution(forward_dict, i)
+    mass_vector_backward = calculate_mass_distribution(backward_dict, i)
+    forward_avg, backward_avg = make_avg_mass(mass_vector_forward, mass_vector_backward)
+    return forward_avg, backward_avg
 
 
 def make_distributions(forward_dict, backward_dict):
     forward_masses = calculate_mass_distribution(forward_dict)
     backward_masses = calculate_mass_distribution(backward_dict)
-    display_masses(forward_masses, backward_masses)
+    make_avg_mass(forward_masses, backward_masses)
+    display_masses(forward_masses, backward_masses, now_results_dir)
 
 
-def display_masses(forward_masses, backward_masses):
-    now_results_dir = results_path / f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
-    Path.mkdir(now_results_dir)
+def make_avg_mass(forward_masses, backward_masses):
+    forward_avg = np.average(forward_masses)
+    backward_avg = np.average(backward_masses)
+    return forward_avg, backward_avg
+
+
+def display_masses(forward_masses, backward_masses, now_results_dir):
     """plt.hist(forward_masses, bins=graph_resolution, color='y')
     plt.title('Forward Mass Distribution')
     plt.show()
@@ -70,22 +112,14 @@ def main(file_path, forward_dict, backward_dict):
         add_to_dict(backward_dict, words[i], words[i - 1])
 
 
-counter_key = 'All_appearances_ooooooo'
-
-graph_resolution = 60
-
-number_of_words = 4
-no_mass = -9999
-
-
 def kl_divergence_from_uniform():
     pass
 
 
-def word_mass(word_dict: dict):
+def word_mass(word_dict: dict, number_of_words=number_of_words_global):
     total = word_dict[counter_key]
     # The distribution of rare words say nothing
-    if total < 30:
+    if total < rare_word_threshold:
         return no_mass
     word_dict.pop(counter_key)
     total_keys = len(word_dict.keys())
@@ -93,11 +127,12 @@ def word_mass(word_dict: dict):
     frequencies_list = sorted(frequencies_list, reverse=True)
     effective_word_number = min(total_keys, number_of_words)
     accumulated_mass = sum(frequencies_list[0:effective_word_number]) / total
+    word_dict[counter_key] = total
     return accumulated_mass
 
 
-def calculate_mass_distribution(bigrams_dict):
-    mass_vector = [word_mass(bigrams_dict[word]) for word in bigrams_dict]
+def calculate_mass_distribution(bigrams_dict, number_of_words=number_of_words_global):
+    mass_vector = [word_mass(bigrams_dict[word], number_of_words) for word in bigrams_dict]
     mass_vector = [el for el in mass_vector if el != no_mass]
     sorted_masses = sorted(mass_vector, reverse=True)
     return sorted_masses
